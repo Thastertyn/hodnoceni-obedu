c8dbec8f58d0445747fafc57e4717540d60bd881
denisheim <104193875+denisheim@users.noreply.github.com>
2025-03-27 19:14:38 +0100

    [WIP] TodayMealCard functionality

--- frontend/src/components/TodayMealCard.tsx
+++ frontend/src/components/TodayMealCard.tsx
@@ -17,13 +17,20 @@ export function TodayMealCard({
 }: TodayMealCardProps) {
 	const navigate = useNavigate();
 
+	const isOrdered = todayMeal?.main_course !== undefined;
+
 	return (
 		<section className="today-meal">
 			<h3 style={{ color: "black" }}>Dnešní oběd</h3>
-			{loading && (
-				<p className="loading-message">Načítání dnešního oběda...</p>
+			{loading && <p className="loading-message">Načítání dnešního oběda...</p>}
+
+			{todayMeal === null && (
+				<div className="error-container">
+					<p className="error-message">Neobjednáno</p>
+				</div>
 			)}
-			{(todayMeal == null || error)&& (
+
+			{error && (
 				<div className="error-container">
 					<p className="error-message">{error}</p>
 					<button onClick={refresh} className="retry-button">
@@ -31,9 +38,26 @@ export function TodayMealCard({
 					</button>
 				</div>
 			)}
+
 			{!loading && !error && todayMeal && (
 				<div className="meal-options">
-					<div className="meal-card">
+					<div
+						className="meal-card"
+						onClick={() =>
+							isOrdered &&
+							navigate("/rating", {
+								state: {
+									date: todayMeal.lunch_date,
+								},
+							})
+						}
+						style={{
+							cursor: isOrdered ? "pointer" : "default",
+						}}
+					>
+						<span className="status" style={{ color: "red" }}>
+							Nehodnoceno
+						</span>
 						<p>
 							<strong>Polévka:</strong> {todayMeal.soup}
 						</p>
@@ -44,17 +68,6 @@ export function TodayMealCard({
 							<strong>Nápoj:</strong> {todayMeal.drink}
 						</p>
 						<small>Dnes</small>
-						{/* <button
-							className="rate-button"
-							onClick={() =>
-								navigate("/rating", {
-									state: {
-										date: todayMeal.lunch_date,
-									},
-								})
-							}>
-							Hodnotit
-						</button> */}
 					</div>
 				</div>
 			)}
